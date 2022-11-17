7a9936e5c6d38cc5e3216018edb26381b24edbc6
NikitaQAP <112653777+NikitaQAP@users.noreply.github.com>
2022-11-17 13:37:06 +0300

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,40 @@
+import telebot
+import requests
+import json
+
+TOKEN = '5707501023:AAHMXFl_ilb0ZhTiVi6MbspYat1eY63KXbA'
+
+bot = telebot.TeleBot(TOKEN)
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD'
+}
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу, введите команду боту в следующем формате:' \
+           '\n<имя валюты>''<в какую валюту перевести>' \
+           '<количество переводимой валюты>' \
+           '\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    quote, base, amount = message.text.split(' ')
+    r = requests.get(f'https://www.cryptocompare.com/')
+    total_base = json.loads(r.content)[keys[base]]
+    text = f'Цена {amount} {quote} в {base} - {total_base}'
+    bot.send_message(message.chat.id, text)
+
+bot.polling()
